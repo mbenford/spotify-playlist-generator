ea9474758014ad9f3802e71e79deecc93c1c19d1
Michael Benford <michael@michaelbenford.net>
2015-03-21 19:17:47 -0300

    Configuração básica do servidor implementada

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
+var express = require('express'),
+    app = express(),
+    router = express.Router();
 
+app.engine('html', require('ejs').renderFile);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.listen(8899, 'localhost');
+console.log('Listening on localhost:8899');
