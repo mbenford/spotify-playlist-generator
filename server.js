9c2087a96b2436ccd14357881092650d6ea0905e
Michael Benford <michael@michaelbenford.net>
2015-03-21 19:42:32 -0300

    Página inicial criada

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ var express = require('express'),
 app.engine('html', require('ejs').renderFile);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
+app.use('/static', express.static(__dirname + '/static'));
+app.use('/', router);
+
+router.get('/', function(req, res) {
+   res.render('index.html');
+});
 
 app.listen(8899, 'localhost');
 console.log('Listening on localhost:8899');
